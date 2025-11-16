56033cf55652b7cc50d582b7a53f2ebfa7053f37
Agustín Conde <conde.agustin66@gmail.com>
2025-11-16 16:27:51 -0300

    feat: añade tabla de User al schema
    Se aplicò la migraciòn para añadir la tabla de usuarios al esquema de la base de datos

--- backend/ProyectHub.Data/Migrations/20251116183812_AddUserTable.Designer.cs
+++ backend/ProyectHub.Data/Migrations/20251116183812_AddUserTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectHub.Data;
 
@@ -10,9 +11,11 @@ using ProyectHub.Data;
 namespace ProyectHub.Data.Migrations
 {
     [DbContext(typeof(ProyectHubDbContext))]
-    partial class ProyectHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251116183812_AddUserTable")]
+    partial class AddUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
