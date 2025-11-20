830f9e32e264a6a389e8cee21aaf688958d05bc2
Agustín Conde <conde.agustin66@gmail.com>
2025-11-20 17:16:12 -0300

    feat: aplica migración para nuevos campos

--- backend/ProyectHub.Data/Migrations/20251120193110_AddAuditLogFields.Designer.cs
+++ backend/ProyectHub.Data/Migrations/20251120193110_AddAuditLogFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectHub.Data;
 
@@ -11,9 +12,11 @@ using ProyectHub.Data;
 namespace ProyectHub.Data.Migrations
 {
     [DbContext(typeof(ProyectHubDbContext))]
-    partial class ProyectHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251120193110_AddAuditLogFields")]
+    partial class AddAuditLogFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/ProyectHub.Data/Migrations/20251120193110_AddAuditLogFields.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectHub.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditLogFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Data",
+                table: "AuditLogs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PerformedByEmail",
+                table: "AuditLogs",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RestoredAt",
+                table: "AuditLogs",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "AuditLogs");
+
+            migrationBuilder.DropColumn(
+                name: "PerformedByEmail",
+                table: "AuditLogs");
+
+            migrationBuilder.DropColumn(
+                name: "RestoredAt",
+                table: "AuditLogs");
+        }
+    }
+}
